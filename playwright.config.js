d7a568c397e0001a2d4f3557b60b279758ecc00a
Josue <josuee.ald@gmail.com>
2026-03-29 18:16:58 -0500

    chore: use chromium as browser for test with playwright

--- playwright.config.js
+++ playwright.config.js
@@ -26,10 +26,6 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    }
   ],
 });
